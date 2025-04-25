21e953a33a2ebd9f82fffcf484b91f7c39b24855
JosephVankelegom <jtevanvan@gmail.com>
2025-04-25 15:00:11 +0200

    correction grid_reconstruction.py functionnne avec des list quand il ne dois pas les re-order, ca va pas si une ligne est mal placer au départ (fonctionne avec vertiacl et horizontal)

--- grid_reconstruction.py
+++ grid_reconstruction.py
@@ -6,12 +6,18 @@ def grid_reconstruction(lines):
             if len(l) > result:
                 result = len(l)
         return result
-    
+
+    '''
+    This function serve to know if an element are already present in the grid.
+    It will return the coordonates in the grid and the coordonate of the first element of the line found in the grid
+    Input : gr => the grid
+    Ouput : inside (bool), coordonates (array) 
+    '''
     def get_pos(gr, li):
         for x in range(len(gr)):
             for y in range(len(gr)):
                 for i in range(len(li)):
-                    if is_equals(grid[y][x],line[i]):
+                    if is_equals(grid[y][x],li[i]):
                         return True, [x,y,i]
         return False, []
     
@@ -56,16 +62,16 @@ def grid_reconstruction(lines):
                 
         for pos, val in enumerate(line):
             if horizontal:
-                pos_grid = [new_pos_g[0] ,pos -pos_l + new_pos_g[1]]
+                pos_grid = [pos -pos_l + new_pos_g[0] ,new_pos_g[1]]
             else:
-                pos_grid = [pos -pos_l + new_pos_g[0], new_pos_g[1]]
+                pos_grid = [new_pos_g[0], pos -pos_l + new_pos_g[1]]
                 
-            if not is_empty(grid[pos_grid[0]][pos_grid[1]]):
+            if not is_equals(grid[pos_grid[1]][pos_grid[0]], val) and not is_empty(grid[pos_grid[1]][pos_grid[0]]):
                 if horizontal:
                     returned_line.append(empty_line(grid, grid_filled, pos_grid[0], False ))
                 else:
                     returned_line.append(empty_line(grid, grid_filled, pos_grid[1], True ))
-            grid[pos_grid[0]][pos_grid[1]] = val
+            grid[pos_grid[1]][pos_grid[0]] = val
         return returned_line
         
     def is_empty(val):
@@ -97,18 +103,24 @@ def grid_reconstruction(lines):
     grid = [[emptyness for _ in range(maxleng)] for _ in range(maxleng)]
     grid_filled = [set({}),set({})] # row and then col
     ret_lines = []
+    empty_place_start= [0,0] # TODO to change ugly
     
     for line in lines:
+
         inside, pos = get_pos(grid, line)
         if not inside:
-            rettt = fill_grid(grid, grid_filled, line, [0,0], 0, True)
+            rettt = fill_grid(grid, grid_filled, line, empty_place_start , 0, True)
+            grid_filled[0].add(empty_place_start[1])
+            empty_place_start[1] += 1
             ret_lines.append(rettt)
         else:
-            if pos[1] in grid_filled[0]:
-                rettt = fill_grid(grid, grid_filled, line, pos[0:2], pos[2], False)
+            if not pos[1] in grid_filled[0]:
+                rettt = fill_grid(grid, grid_filled, line, pos[0:2], pos[2], True)
+                grid_filled[0].add(pos[1])
                 ret_lines.append(rettt)
             else:
-                rettt = fill_grid(grid, grid_filled, line, pos[0:2], pos[2], True)
+                rettt = fill_grid(grid, grid_filled, line, pos[0:2], pos[2], False)
+                grid_filled[1].add(pos[0])
                 ret_lines.append(rettt)
     for ll in grid:
         print(ll)
@@ -116,9 +128,9 @@ def grid_reconstruction(lines):
 
 lines_exemple = [[[0,0],[0,1],[0,2]]
                  ,[[1,0],[1,1],[1,2]]
+                 ,[[0,1],[1,1],[2,1]]
                  ,[[2,0],[2,1],[2,2]]
-                 ,[[3,0],[3,1],[3,2]]
-                 ,[[1,1],[2,1],[3,1]]]
+                 ,[[0,2],[1,2],[2,2]]]
 grid_reconstruction(lines_exemple)
 
 
